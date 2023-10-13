86113aca5011258d6603cef6bcd0f8b216698b0b
DESKTOP-RB4RBG5\timoj <timonsteri.joensuu@gmail.com>
2023-10-13 23:25:40 +0300

    no message

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineSubsystem.h"
+#include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MenuSystem/MenuInterface.h"
+#include "PuzzlePlatformsGameInstance.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance, public IMenuInterface
+{
+	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer);
+
+	virtual void Init();
+
+	UFUNCTION(BlueprintCallable)
+	void LoadMenu();
+
+	UFUNCTION(BlueprintCallable)
+	void InGameLoadMenu();
+
+	UFUNCTION(Exec)
+	void Host() override;
+
+	UFUNCTION(Exec)
+	void Join(uint32 Index) override;
+
+	virtual void LoadMainMenu() override;
+
+	void RefreshServerList() override;
+
+private:
+	TSubclassOf<class UUserWidget> MenuClass;
+	TSubclassOf<class UUserWidget> InGameMenuClass;
+
+	class UMainMenu* Menu;
+	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+
+	void OnCreateSessionComplete(FName SessionName, bool Success);
+	void OnDestroySessionComplete(FName SessionName, bool Success);
+	void OnFindSessionsComplete(bool Success);
+	void CreateSession();
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	
+};
